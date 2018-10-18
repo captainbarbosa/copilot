5a28f00e4782924b0016e5ff6fe7b1e512cb2580
Nadia Barbosa <nadiabarbosa@me.com>
2018-10-18 13:16:59 -0700

    Add watch target

---
+++ copilot-watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  copilot-watch Extension
+//
+//  Created by Nadia Barbosa on 10/18/18.
+//  Copyright © 2018 Nadia Barbosa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
