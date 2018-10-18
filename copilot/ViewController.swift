e3ec1e890e7f96829dcb38b245e5eb80c04d1534
Nadia Barbosa <nadiabarbosa@me.com>
2018-10-18 12:03:26 -0700

    Add app setup

--- copilot/ViewController.swift
+++ copilot/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Mapbox
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
+    
+    var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView = MGLMapView(frame: view.bounds)
+        mapView.delegate = self
+        mapView.userTrackingMode = .follow
+        view.addSubview(mapView)
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(handleMapTap(sender:)))
+        for recognizer in mapView.gestureRecognizers! where recognizer is UITapGestureRecognizer {
+            singleTap.require(toFail: recognizer)
+        }
+        mapView.addGestureRecognizer(singleTap)
     }
-
-
+    
+    @objc @IBAction func handleMapTap(sender: UITapGestureRecognizer) {
+        
+        if let annotations = mapView.annotations {
+            mapView.removeAnnotations(annotations)
+        }
+        
+        let tapPoint: CGPoint = sender.location(in: mapView)
+        let annotation = MGLPointAnnotation()
+        annotation.title = "Navigate here!"
+        annotation.coordinate = mapView.convert(tapPoint, toCoordinateFrom: mapView)
+        mapView.addAnnotation(annotation)
+    }
+    
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
+        guard let origin = mapView.userLocation?.coordinate else { return }
+        let destination = annotation.coordinate
+        var navigationRoute: Route!
+        
+        let options = NavigationRouteOptions(coordinates: [origin, destination], profileIdentifier: .automobileAvoidingTraffic)
+        
+        _ = Directions.shared.calculate(options, completionHandler: { (waypoints, routes, error) in
+            navigationRoute = routes?.first
+            
+            let navigationViewController = NavigationViewController(for: navigationRoute!)
+            self.present(navigationViewController, animated: true, completion: nil)
+        })
+    }
 }
 
